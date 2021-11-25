cab315236eb4514af3b718076c573f1753e7aa1c
mrkcsc <miguel@miguelgaeta.com>
2021-11-24 17:16:53 -0800

    Re-work flow to not OOM.

--- android/src/main/java/com/github/yamill/orientation/listeners/OrientationAutoRotateListener.kt
+++ android/src/main/java/com/github/yamill/orientation/listeners/OrientationAutoRotateListener.kt
@@ -18,7 +18,28 @@ internal class OrientationAutoRotateListener(
     private val autoRotate
         get() = Settings.System.getInt(reactContext.contentResolver, autoRotateFlag, 0) == 1
 
-    private val observer: ContentObserver = object : ContentObserver(Handler(Looper.myLooper()!!)) {
+    private var observer: ContentObserver? = null
+
+    init {
+        onAutoRotateEnabled(autoRotate)
+    }
+
+    override fun onHostResume() {
+        observer = createContentObserver()
+        observer?.let { reactContext.contentResolver.registerContentObserver(setting, false, it) }
+
+        onAutoRotateEnabled(autoRotate)
+    }
+
+    override fun onHostPause() {
+        observer?.let { reactContext.contentResolver.unregisterContentObserver(it) }
+        observer = null
+    }
+
+    override fun onHostDestroy() {}
+
+    private fun createContentObserver() = object : ContentObserver(Handler(Looper.myLooper()!!)) {
+
         override fun onChange(selfChange: Boolean) {
             super.onChange(selfChange)
 
@@ -29,18 +50,4 @@ internal class OrientationAutoRotateListener(
             return true
         }
     }
-
-    init {
-        onAutoRotateEnabled(autoRotate)
-    }
-
-    override fun onHostResume() {
-        reactContext.contentResolver.registerContentObserver(setting, false, observer)
-    }
-
-    override fun onHostPause() {
-        reactContext.contentResolver.unregisterContentObserver(observer)
-    }
-
-    override fun onHostDestroy() {}
 }
